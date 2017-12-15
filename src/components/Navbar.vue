f4eebbd574c50c089bf38d262dc974b8659ebdaa
Luk Szw <lukaszwedcrypto@gmail.com>
2017-12-15 00:49:59 +0000

    Dev complete

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <div class="nav-wrapper green"> 
+      <div class="cointainer">
+        <router-link to="/" class="brand-logo">Employee Manager</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDkuGcbc8hek-3MQIThmn8WWA6tFBSAV7w",
+  authDomain: "vuefs-prod-6b07e.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-6b07e.firebaseio.com",
+  projectId: "vuefs-prod-6b07e",
+  storageBucket: "vuefs-prod-6b07e.appspot.com",
+  messagingSenderId: "636604117818"
+}
